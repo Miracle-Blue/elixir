65a61918e6093e217a13d643a9dc128c884fbdda
Miracle-Blue <miracle.blue.com@gmail.com>
2025-10-14 17:39:27 +0500

    feat: Add ElixirControllerExtension to manage navigation state changes and enforce page uniqueness

--- lib/src/util/context_extension.dart
+++ lib/src/util/context_extension.dart
@@ -1,7 +1,30 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
+import '../navigator/elixir_page.dart';
 import '../navigator/navigator.dart';
 
 extension ElixirContextExtension on BuildContext {
   ElixirState get elixir => Elixir.of(this);
+}
+
+extension ElixirControllerExtension on ValueNotifier<ElixirNavigationState> {
+  void change(ElixirNavigationState Function(ElixirNavigationState pages) fn) {
+    final prev = value.toList();
+    var next = fn(prev);
+    if (next.isEmpty) return;
+
+    var haveSamePage = false;
+    for (var i = 0; i < value.length; i++) {
+      if (next.where((e) => e == value[i]).length > 1) {
+        haveSamePage = true;
+        break;
+      }
+    }
+    assert(haveSamePage, 'Didn\'t pass the same page');
+    if (next.isEmpty || haveSamePage) return;
+
+    value = UnmodifiableListView<ElixirPage>(next);
+  }
 }
