01229056448d5f3d7925640668377a41bdf57e5f
Miracle-Blue <miracle.blue.com@gmail.com>
2025-10-14 17:33:54 +0500

    refactor: Remove unused ElixirControllerExtension and clean up context_extension.dart for improved clarity

--- lib/src/util/context_extension.dart
+++ lib/src/util/context_extension.dart
@@ -1,21 +1,7 @@
-import 'dart:collection';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import '../navigator/elixir_page.dart';
 import '../navigator/navigator.dart';
 
 extension ElixirContextExtension on BuildContext {
   ElixirState get elixir => Elixir.of(this);
-}
-
-extension ElixirControllerExtension on ValueNotifier<ElixirNavigationState> {
-  void change(ElixirNavigationState Function(ElixirNavigationState pages) fn) {
-    final prev = value.toList();
-    var next = fn(prev);
-    if (next.isEmpty) return;
-    if (next.isEmpty || listEquals(next, value)) return;
-    value = UnmodifiableListView<ElixirPage>(next);
-  }
 }
