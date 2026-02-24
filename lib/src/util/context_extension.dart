ef6073347b521e5ad1692bf0993922d7cfcde820
Miracle-Blue <miracle.blue.com@gmail.com>
2026-02-24 11:00:38 +0500

    chore: Bump version number to 0.1.1 in pubspec.yaml and add popUntil method to ElixirControllerExtension for improved navigation management

--- lib/src/util/context_extension.dart
+++ lib/src/util/context_extension.dart
@@ -11,10 +11,16 @@ extension ElixirContextExtension on BuildContext {
 }
 
 extension ElixirControllerExtension on ValueNotifier<ElixirNavigationState> {
+  /// Change the navigation state.
   void change(ElixirNavigationState Function(ElixirNavigationState pages) fn) {
     final prev = value.toList();
     var next = fn(prev);
     if (next.isEmpty || listEquals(next, value)) return;
     value = UnmodifiableListView<ElixirPage>(next);
   }
+
+  /// Pop until the last page with the given tag.
+  void popUntil(ElixirAlias tag) => change(
+    (state) => [...state.takeWhile((page) => page.alias != tag), state.firstWhere((page) => page.alias == tag)],
+  );
 }
